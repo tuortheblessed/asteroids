1c5fcd2ba010d1f74959d6cdc1c22bf80071ba2f
TuorTheBlessed <kyle.dellis@protonmail.com>
2024-12-04 16:54:21 -0600

    added player movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
@@ -9,17 +10,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        surface.fill("black")
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
